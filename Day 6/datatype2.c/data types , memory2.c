510d357b41be28b29d9a7f2ce2837dfe9584ae15
Dhananjaya <dhananjayab538@gmail.com>
2026-08-14 22:01:35 +0530

    Merge branch 'main' of https://github.com/Dhanan0012/software-learning---C

---
+++ Day 6/datatype2.c/data types , memory2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+   int a = 10;
+   float b = 10000;
+
+
+
+    printf("int: %zu \n", sizeof(a));
+    printf("int: %zu \n", sizeof(b));
+  
+    return 0;
+  
+} 
